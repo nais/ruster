c52da839e32aa2ee5e14f2fa8dd4a7bb9b291ce4
x10an14-nav <christian.chavez@nav.no>
2024-06-13 13:02:29 +0200

    example showing ownership, borrowing, and mutation

--- src/main.rs
+++ src/main.rs
@@ -10,4 +10,24 @@ fn main() {
     println!("Hello, world!");
     mymodule::hello_world();
     mymodule::mysubmodule::hello_world2();
+
+    // Immutable vector
+    let my_vector = Vec::from([1, 1, 2, 3, 5, 8]);
+    // Mutable vector, based off of an immutable one
+    let mut new_vector = &mut return_new_fibonacci_13(&my_vector);
+    new_vector = add_fibonacci_13(new_vector);
+
+    // Happen to have the same contents!
+    assert!(my_vector == *new_vector);
+}
+
+/// Return new vector
+fn return_new_fibonacci_13(my_vector: &[i32]) -> Vec<i32> {
+    [my_vector, &[13]].concat().to_vec()
+}
+
+/// Return the same vector
+fn add_fibonacci_13(my_vector: &mut Vec<i32>) -> &mut Vec<i32> {
+    my_vector.push(13);
+    my_vector
 }
