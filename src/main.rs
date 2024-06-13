e82f5e7166648fe38b85d1abd5d6cdac4514b9be
x10an14-nav <christian.chavez@nav.no>
2024-06-13 12:22:05 +0200

    feature: Add arbitrarily nested submodules

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+mod mymodule;
+
 fn main() {
     println!("Hello, world!");
+    mymodule::hello_world();
+    mymodule::mysubmodule::hello_world2();
 }
---
+++ src/mymodule/mysubmodule.rs
@@ -0,0 +1,3 @@
+pub(crate) fn hello_world2() -> String {
+    String::from("Hello world2!")
+}
