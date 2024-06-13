79a28b2a09c8e2e828852c14ac7c9e9d7446c287
x10an14-nav <christian.chavez@nav.no>
2024-06-13 12:32:08 +0200

    example: inline module!

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
 mod mymodule;
 
+mod myinlinemodule {
+    pub(crate) fn hello_world() -> String {
+        "Hello, world!".into()
+    }
+}
+
 fn main() {
     println!("Hello, world!");
     mymodule::hello_world();
