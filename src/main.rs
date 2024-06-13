16e61ceda563d27690438755408100eb8e80a7da
Kim Tore Jensen <kimtjen@gmail.com>
2024-06-13 15:06:11 +0200

    hello world web service

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,28 @@
+use axum::{Json, Router};
+use axum::routing::post;
+use serde::{Deserialize, Serialize};
+
 mod old;
 mod mymodule;
 mod fib;
 
 #[tokio::main]
 async fn main() {
-    // webapp
-    // /foo (request) -> json(response)
+    let router = Router::new().route("/", post(handler));
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:6000").await.unwrap();
+    axum::serve(listener, router).await.unwrap();
+}
+
+#[derive(Deserialize)]
+struct HelloRequest {
+    pub msg: String
+}
+
+#[derive(Serialize)]
+struct HelloResponse {
+    pub msg: String
+}
+
+async fn handler(Json(payload): Json<HelloRequest>) -> Json<HelloResponse> {
+    Json(HelloResponse{msg: format!("you said: {}", payload.msg).into()})
 }
